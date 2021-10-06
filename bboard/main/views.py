e2f5bba9f2388fd3cf14856fda9b0ad817760d0e
allprecisely <allprecisely@gmail.com>
2021-10-06 17:03:44 +0300

    add about page

--- bboard/main/views.py
+++ bboard/main/views.py
@@ -1,5 +1,17 @@
+from django.http import Http404
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.template import TemplateDoesNotExist
+from django.template.loader import get_template
 
 
 def index(request):
     return render(request, 'main/index.html')
+
+
+def other_page(request, page):
+    try:
+        template = get_template('main/' + page + '.html')
+    except TemplateDoesNotExist:
+        raise Http404
+    return HttpResponse(template.render(request=request))
