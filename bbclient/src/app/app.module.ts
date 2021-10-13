9538c5ea0657d432e417fec300ed3f4461e4b13d
allprecisely <allprecisely@gmail.com>
2021-10-13 12:47:15 +0300

    gen components and modules

--- bbclient/src/app/app.module.ts
+++ bbclient/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BbListComponent } from './bb-list.component';
+import { BbDetailComponent } from './bb-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BbListComponent,
+    BbDetailComponent
   ],
   imports: [
     BrowserModule
