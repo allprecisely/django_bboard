6347f28c1c55a279c2ac7184e98270ad0f09fefc
allprecisely <allprecisely@gmail.com>
2021-10-18 18:16:25 +0300

    static

--- travelblog/travelblog/settings.py
+++ travelblog/travelblog/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -24,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ld=#k5li1ebt1r$!kx@kh2)$q2pf-%s143u$6#hdwqf@!3j@vx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -156,6 +155,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = BASE_DIR / 'static'
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
--- travelblog/utils/entrypoint.sh
+++ travelblog/utils/entrypoint.sh
@@ -4,4 +4,6 @@ python3 manage.py makemigrations --no-input
 
 python3 manage.py migrate --no-input
 
-exec gunicorn -c "${PROJECT_DIR}/gunicorn_config.py" travelblog.wsgi
+python3 manage.py collectstatic --no-input
+
+exec gunicorn -c "${PROJECT_DIR}/gunicorn_config.py" travelblog.wsgi --reload
