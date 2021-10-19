d984d863bf4485426aa236a4f37d3498e2881f46
allprecisely <allprecisely@gmail.com>
2021-10-19 13:23:48 +0300

    configs

--- travelblog/gunicorn_config.py
+++ travelblog/gunicorn_config.py
@@ -1,6 +1,10 @@
-command = '/home/gii/django_travelblog/venv/bin/gunicorn'
-pythonpath = '/home/gii/django_travelblog/travelblog'
+import os
+
+from decouple import config
+
+command = os.path.join(config('VENV_DIR'), 'bin', 'gunicorn')
+pythonpath = config('PROJECT_DIR')
 bind = '127.0.0.1:8001'
 workers = 3
-user = 'gii'
+user = config('USER')
 raw_env = 'DJANGO_SETTINGS_MODULE=travelblog.settings'
